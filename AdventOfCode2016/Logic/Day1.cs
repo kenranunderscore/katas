ae75206d1e2f5b9cb2dffcf797cef19f7b9de084
kenranunderscore <johb.maier@gmail.com>
2017-05-22 00:15:25 +0200

    Added first day of AoC 2016

---
+++ AdventOfCode2016/Logic/Day1.cs
@@ -0,0 +1,70 @@
+﻿namespace Logic
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Windows;
+    using System.Windows.Media;
+
+    public class Day1
+    {
+        private static readonly Matrix right = new Matrix(0, -1, 1, 0, 0, 0);
+        private static readonly Matrix left = new Matrix(0, 1, -1, 0, 0, 0);
+        private readonly string input;
+
+        public Day1(string input)
+        {
+            this.input = input;
+        }
+
+        public int Distance()
+        {
+            Vector currentDirection = new Vector(0, 1);
+            Vector currentPosition = new Vector(0, 0);
+
+            var commands =
+                input
+                    .Split(new[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(x => new Tuple<Matrix, int>(x[0] == 'R' ? right : left, int.Parse(x.Substring(1))));
+            foreach (var command in commands)
+            {
+                currentDirection = AdvanceDirection(currentDirection, command);
+                currentPosition += currentDirection * command.Item2;
+            }
+
+            return DistanceFromOrigin(currentPosition);
+        }
+
+        public int DistanceToFirstDoublyVisitedLocation()
+        {
+            Vector currentDirection = new Vector(0, 1);
+            Vector currentPosition = new Vector(0, 0);
+            HashSet<Vector> visitedLocations = new HashSet<Vector> { currentPosition };
+
+            var commands =
+                input
+                    .Split(new[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(x => new Tuple<Matrix, int>(x[0] == 'R' ? right : left, int.Parse(x.Substring(1))));
+            foreach (var command in commands)
+            {
+                currentDirection = AdvanceDirection(currentDirection, command);
+                for (int i = 0; i < command.Item2; ++i)
+                {
+                    currentPosition += currentDirection;
+                    if (visitedLocations.Contains(currentPosition))
+                    {
+                        return DistanceFromOrigin(currentPosition);
+                    }
+
+                    visitedLocations.Add(currentPosition);
+                }
+            }
+
+            return DistanceFromOrigin(currentPosition);
+        }
+
+        private static Vector AdvanceDirection(Vector direction, Tuple<Matrix, int> command) => Vector.Multiply(direction, command.Item1);
+
+        private static int DistanceFromOrigin(Vector position) => (int)(Math.Abs(position.X) + Math.Abs(position.Y));
+    }
+}
---
+++ AdventOfCode2016/Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("183791f3-bc48-4e92-87b5-76fff9a65db9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
