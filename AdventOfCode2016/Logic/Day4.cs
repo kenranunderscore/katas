6872b22fe0186583315a07fa1a290fc177fc724e
kenranunderscore <johb.maier@gmail.com>
2017-08-26 14:26:45 +0200

    Add AoC2016 day 4

---
+++ AdventOfCode2016/Logic/Day4.cs
@@ -0,0 +1,62 @@
+﻿namespace Logic
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    public class Day4 : DayWithInput<IEnumerable<string>>
+    {
+        public Day4(IEnumerable<string> input) : base(input)
+        {
+        }
+
+        public int SumOfSectorIdsOfRealRooms => input
+            .Where(IsRoomReal)
+            .Sum(ExtractSectorIdFromRoom);
+
+        public int NorthPoleStorageSectorId => ExtractSectorIdFromRoom(input
+            .Where(IsRoomReal)
+            .Select(DecryptRealRoom)
+            .Single(r => r.Contains("northpole")));
+
+        internal static string DecryptRealRoom(string room)
+        {
+            int sectorId = ExtractSectorIdFromRoom(room);
+            return new string(room
+                .Select(r => Rotate(r, sectorId))
+                .ToArray());
+        }
+
+        internal static char Rotate(char c, int count)
+        {
+            int last = 'z';
+            int first = 'a';
+            if (c < first || c > last)
+                return c;
+
+            int diff = count % 26;
+            int sum = (int)c + diff;
+            if (sum > last)
+                sum -= 26;
+
+            return (char)sum;
+        }
+
+        internal static int ExtractSectorIdFromRoom(string room) => int.Parse(Regex.Match(room, @"\-(\d+)\[").Groups[1].Value);
+
+        internal static bool IsRoomReal(string room)
+        {
+            string checksum = Regex.Match(room, @"\[(.*)\]").Groups[1].Value;
+            string name = room.Substring(0, room.IndexOf('['));
+            string strippedName = Regex.Replace(name, @"[-\d]", string.Empty);
+            var correctChecksum = new string(strippedName
+                .GroupBy(c => c, (c, g) => new { Char = c, Quantity = g.Count() })
+                .OrderByDescending(x => x.Quantity)
+                .ThenBy(x => x.Char)
+                .Take(5)
+                .Select(x => x.Char)
+                .ToArray());
+            return checksum == correctChecksum;
+        }
+    }
+}
