4a246c61396a95ca11ee627b30f50d5908b5f4d9
kenranunderscore <johb.maier@gmail.com>
2017-08-27 15:58:33 +0200

    Add AoC 2k16 day 6 part 1

---
+++ AdventOfCode2016/Logic/Day6.cs
@@ -0,0 +1,32 @@
+﻿namespace Logic
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Day6 : DayWithInput<IEnumerable<string>>
+    {
+        public Day6(IEnumerable<string> input)
+            : base(input)
+        {
+        }
+
+        public string DecipherMessage()
+        {
+            int columnCount = input.First().Length;
+            StringBuilder sb = new StringBuilder(columnCount);
+            for (int i = 0; i < columnCount; i++)
+            {
+                var column = input.Select(s => s[i]);
+                var mostFrequentCharacter = column
+                    .GroupBy(c => c, (c, g) => (c, g.Count()))
+                    .OrderByDescending(c => c.Item2)
+                    .First()
+                    .c;
+                sb.Append(mostFrequentCharacter);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ AdventOfCode2016/Tests/Day6_Test.cs
@@ -0,0 +1,22 @@
+﻿namespace Tests
+{
+    using Logic;
+    using NUnit.Framework;
+    using Tests.Inputs;
+
+    [TestFixture]
+    public class Day6_Test
+    {
+        [Test]
+        public void Day6_Part1()
+        {
+            var day6 = new Day6(Utils.ReadLines("day6_data.txt"));
+            Assert.That(day6.DecipherMessage(), Is.EqualTo("ygjzvzib"));
+        }
+
+        [Test]
+        public void Day6_Part2()
+        {
+        }
+    }
+}
