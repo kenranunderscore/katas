e1cdc56833fe6543d8a0ad0f78fdd11530eab9a4
kenranunderscore <johb.maier@gmail.com>
2017-08-27 21:47:28 +0200

    Add AoC 2k16 day 7 part 2

--- AdventOfCode2016/Logic/Day7.cs
+++ AdventOfCode2016/Logic/Day7.cs
@@ -12,17 +12,52 @@
 
         public int NumberOfIPsThatSupportTLS => input.Count(SupportsTLS);
 
-        internal static bool SupportsTLS(string val)
+        public int NumberOfIPsThatSupportSSL => input.Count(SupportsSSL);
+
+        internal static bool SupportsSSL(string sequence)
         {
-            foreach (var match in bracketContentRegex.Matches(val).Cast<Match>())
+            var abas = ExtractABAs(sequence);
+
+            foreach (string aba in abas)
+            {
+                string bab = new string(new[] { aba[1], aba[0], aba[1] });
+                foreach (var match in bracketContentRegex.Matches(sequence).Cast<Match>())
+                {
+                    if (match.Groups[1].Value.Contains(bab))
+                        return true;
+                }
+            }
+
+            return false;
+        }
+
+        internal static bool SupportsTLS(string sequence)
+        {
+            foreach (var match in bracketContentRegex.Matches(sequence).Cast<Match>())
             {
                 if (HasABBA(match.Groups[1].Value))
                     return false;
             }
 
-            return val.Split('[', ']')
+            return sequence.Split('[', ']')
                 .Where((s, i) => i % 2 == 0)
                 .Any(HasABBA);
+        }
+
+        private static IReadOnlyCollection<string> ExtractABAs(string val)
+        {
+            var abas = new List<string>();
+            foreach (string s in val.Split('[', ']').Where((s, i) => i % 2 == 0))
+            {
+                for (int i = 0; i <= s.Length - 3; i++)
+                {
+                    string str = s.Substring(i, 3);
+                    if (str[0] != str[1] && str[0] == str[2])
+                        abas.Add(str);
+                }
+            }
+
+            return abas;
         }
 
         private static bool HasABBA(string val)
--- AdventOfCode2016/Tests/Day7_Test.cs
+++ AdventOfCode2016/Tests/Day7_Test.cs
@@ -15,9 +15,28 @@
         }
 
         [Test]
-        public void Sample_String_That_Support_TLS_Is_Judged_Correctly()
+        public void Day7_Part2()
+        {
+            var day7 = new Day7(Utils.ReadLines("day7_data.txt"));
+            Assert.That(day7.NumberOfIPsThatSupportSSL, Is.EqualTo(231));
+        }
+
+        [Test]
+        public void Sample_String_That_Supports_TLS_Is_Judged_Correctly()
         {
             Assert.That(Day7.SupportsTLS("abba[mnop]qrst"));
+        }
+
+        [Test]
+        public void Sample_String_That_Supports_SLS_Is_Judged_Correctly()
+        {
+            Assert.That(Day7.SupportsSSL("zazbz[bzb]cdb"));
+        }
+
+        [Test]
+        public void Sample_String_That_Does_Not_Support_SLS_Is_Judged_Correctly()
+        {
+            Assert.That(Day7.SupportsSSL("xyx[xyx]xyx"), Is.False);
         }
     }
 }
