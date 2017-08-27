e23a812fe3d3a4c56dbeb7ab6f0788c0a1658336
kenranunderscore <johb.maier@gmail.com>
2017-08-27 12:06:13 +0200

    Add day 5 part 1

---
+++ AdventOfCode2016/Logic/Day5.cs
@@ -0,0 +1,47 @@
+﻿namespace Logic
+{
+    using System.Security.Cryptography;
+    using System.Text;
+
+    public class Day5 : DayWithInput<string>
+    {
+        public Day5(string input)
+            : base(input)
+        {
+        }
+
+        public string DecodePassword()
+        {
+            string password = string.Empty;
+            int i = 0;
+            while (password.Length < 8)
+            {
+                string hash = HexMD5Hash($"{input}{i}");
+                if (hash.StartsWith("00000"))
+                {
+                    password += hash[5];
+                }
+                ++i;
+            }
+
+            return password;
+        }
+
+        private static string HexMD5Hash(string value)
+        {
+            using (var md5 = MD5.Create())
+            {
+                var buf = Encoding.UTF8.GetBytes(value);
+                var hash = md5.ComputeHash(buf);
+
+                var builder = new StringBuilder();
+                for (int i = 0; i < hash.Length; ++i)
+                {
+                    builder.Append(hash[i].ToString("x2"));
+                }
+
+                return builder.ToString();
+            }
+        }
+    }
+}
---
+++ AdventOfCode2016/Tests/Day5_Test.cs
@@ -0,0 +1,16 @@
+﻿namespace Tests
+{
+    using Logic;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class Day5_Test
+    {
+        [Test]
+        public void Day5_Part1()
+        {
+            Day5 day5 = new Day5("uqwqemis");
+            Assert.That(day5.DecodePassword(), Is.EqualTo("1a3099aa"));
+        }
+    }
+}
