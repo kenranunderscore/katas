98aa8abf96041db5f3bc0305e743d6536dfae6b9
kenranunderscore <johb.maier@gmail.com>
2017-08-30 23:00:48 +0200

    Add AoC 2k16 day 9 part 1

---
+++ AdventOfCode2016/Logic/Day9.cs
@@ -0,0 +1,45 @@
+﻿namespace Logic
+{
+    using System.Linq;
+    using System.Text;
+
+    public static class Day9
+    {
+        public static string Decompress(string input)
+        {
+            StringBuilder sb = new StringBuilder();
+            int i = 0;
+            while (i < input.Length)
+            {
+                string marker;
+                if (input[i] == '(')
+                {
+                    int closing = input.IndexOf(')', i);
+                    int difference = closing - i - 1;
+                    marker = input.Substring(i + 1, closing - i - 1);
+                    var markerNumbers = marker.Split('x').Select(int.Parse).ToArray();
+                    string target = input.Substring(closing + 1, markerNumbers[0]);
+                    sb.Append(Repeat(target, markerNumbers[1]));
+                    i = closing + 1 + markerNumbers[0];
+                }
+                else
+                {
+                    sb.Append(input[i]);
+                    i++;
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        private static string Repeat(string s, int count)
+        {
+            StringBuilder sb = new StringBuilder(count * s.Length);
+            for (int i = 0; i < count; i++)
+            {
+                sb.Append(s);
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ AdventOfCode2016/Tests/Day9_Test.cs
@@ -0,0 +1,30 @@
+﻿namespace Tests
+{
+    using Logic;
+    using NUnit.Framework;
+    using System.Linq;
+    using Tests.Inputs;
+
+    [TestFixture]
+    public class Day9_Test
+    {
+        [Test]
+        public void Day9_Part1()
+        {
+            string decompressed = Day9.Decompress(Utils.ReadLines("day9_data.txt").First());
+            Assert.That(decompressed.Length, Is.EqualTo(110346));
+        }
+
+        [Test]
+        public void Decompress_Yields_Correct_Result_For_Sample_Without_Marker()
+        {
+            Assert.That(Day9.Decompress("ADVENT"), Is.EqualTo("ADVENT"));
+        }
+
+        [Test]
+        public void Decompress_Yields_Correct_Result_For_Complex_Sample()
+        {
+            Assert.That(Day9.Decompress("X(8x2)(3x3)ABCY"), Is.EqualTo("X(3x3)ABC(3x3)ABCY"));
+        }
+    }
+}
