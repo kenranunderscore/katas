ce343f8a6cf013f615b4e5e4ad40795194d654df
kenranunderscore <johb.maier@gmail.com>
2017-08-31 19:44:20 +0200

    Add AoC 2k16 day 9 part 2

--- AdventOfCode2016/Logic/Day9.cs
+++ AdventOfCode2016/Logic/Day9.cs
@@ -2,6 +2,7 @@
 {
     using System.Linq;
     using System.Text;
+    using System.Text.RegularExpressions;
 
     public static class Day9
     {
@@ -30,6 +31,33 @@
             }
 
             return sb.ToString();
+        }
+
+        public static long FullyDecompressedLength(string input)
+        {
+            long length = 0;
+            int i = 0;
+            while (i < input.Length)
+            {
+                string marker;
+                if (input[i] == '(')
+                {
+                    int closing = input.IndexOf(')', i);
+                    int difference = closing - i - 1;
+                    marker = input.Substring(i + 1, closing - i - 1);
+                    var markerNumbers = marker.Split('x').Select(int.Parse).ToArray();
+                    string target = input.Substring(closing + 1, markerNumbers[0]);
+                    length += FullyDecompressedLength(target) * markerNumbers[1];
+                    i = closing + 1 + markerNumbers[0];
+                }
+                else
+                {
+                    length++;
+                    i++;
+                }
+            }
+
+            return length;
         }
 
         private static string Repeat(string s, int count)
