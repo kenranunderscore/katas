c87c976044b80484e0381cf28ae18c6ddea82f5f
kenranunderscore <johb.maier@gmail.com>
2017-08-31 20:52:40 +0200

    Make day 2 compatible with  .NET Core 2.0

---
+++ AdventOfCode2k16/Tests/Day2_Test.cs
@@ -0,0 +1,26 @@
+﻿namespace Tests
+{
+    using Logic;
+    using NUnit.Framework;
+    using Tests.Inputs;
+
+    [TestFixture]
+    public class Day2_Test
+    {
+        [Test]
+        public void Day2_Part1()
+        {
+            var input = Utils.ReadLines("day2_data.txt");
+            Day2 day2 = new Day2(input);
+            Assert.That(day2.DecipherCode(Day2.NumpadMapping), Is.EqualTo("92435"));
+        }
+
+        [Test]
+        public void Day2_Part2()
+        {
+            var input = Utils.ReadLines("day2_data.txt");
+            Day2 day2 = new Day2(input);
+            Assert.That(day2.DecipherCode(Day2.Part2Mapping), Is.EqualTo("C1A88"));
+        }
+    }
+}
