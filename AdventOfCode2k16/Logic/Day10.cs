d08448d802d053d4cd6d583c6405f00f085c8115
kenranunderscore <johb.maier@gmail.com>
2017-09-01 22:48:21 +0200

    Add AoC 2k16 day 10 part 1 (with heavy reddit inspiration)

---
+++ AdventOfCode2k16/Logic/Day10.cs
@@ -0,0 +1,76 @@
+﻿namespace Logic
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    public class Day10 : DayWithInput<IEnumerable<string>>
+    {
+        Regex valueRegex = new Regex(@"value (?<val>\d+) goes to bot (?<id>\d+)");
+        Regex botRegex = new Regex(@"bot (?<id>\d+) gives low to (?<lowTargetType>(bot|output)) (?<lowId>\d+) and high to (?<highTargetType>(bot|output)) (?<highId>\d+)");
+
+        private readonly Dictionary<int, Action<int>> bots = new Dictionary<int, Action<int>>();
+        private readonly int[] outputs = new int[21];
+
+        public Day10(IEnumerable<string> input) : base(input) { }
+
+        public int FindBotHandlingNumbers(int min, int max)
+        {
+            int targetId = -1;
+
+            foreach (var match in input.Select(l => botRegex.Match(l)).Where(m => m.Success))
+            {
+                int id = int.Parse(match.Groups["id"].Value);
+                List<int> numbers = new List<int>();
+                bots[id] = num =>
+                {
+                    numbers.Add(num);
+                    if (numbers.Count == 2)
+                    {
+                        int low = numbers.Min();
+                        int high = numbers.Max();
+
+                        if (low == min && high == max)
+                        {
+                            targetId = id;
+                        }
+
+                        var lowTargetType = match.Groups["lowTargetType"].Value;
+                        int lowId = int.Parse(match.Groups["lowId"].Value);
+                        var highTargetType = match.Groups["highTargetType"].Value;
+                        int highTargetId = int.Parse(match.Groups["highId"].Value);
+
+                        if (lowTargetType == "output")
+                        {
+                            outputs[lowId] = low;
+                        }
+                        else
+                        {
+                            bots[lowId](low);
+                        }
+
+                        if (highTargetType == "output")
+                        {
+                            outputs[highTargetId] = high;
+                        }
+                        else
+                        {
+                            bots[highTargetId](high);
+                        }
+                    }
+                };
+            }
+
+            foreach (var match in input.Select(l => valueRegex.Match(l)).Where(m => m.Success))
+            {
+                int id = int.Parse(match.Groups["id"].Value);
+                int value = int.Parse(match.Groups["val"].Value);
+                bots[id](value);
+            }
+
+            return targetId;
+        }
+    }
+}
---
+++ AdventOfCode2k16/Tests/Day10_Test.cs
@@ -0,0 +1,17 @@
+﻿namespace Tests
+{
+    using Logic;
+    using NUnit.Framework;
+    using Tests.Inputs;
+
+    [TestFixture]
+    public class Day10_Test
+    {
+        [Test]
+        public void Sample_Bot_2_Values_Are_Found_Correctly()
+        {
+            var day10 = new Day10(Utils.ReadLines("day10_data.txt"));
+            Assert.That(day10.FindBotHandlingNumbers(17, 61), Is.EqualTo(113));
+        }
+    }
+}
