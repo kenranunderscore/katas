b1dccbad8e1b85e3680bf6d30e66276dc45d9eb8
kenranunderscore <johb.maier@gmail.com>
2018-12-15 12:37:06 +0100

    Add day 1 part 2

--- aoc2k18/src/aoc2k18/day_1.clj
+++ aoc2k18/src/aoc2k18/day_1.clj
@@ -1,11 +1,35 @@
 (ns aoc2k18.day-1
   (:require [clojure.string :as string]))
 
+;; part 1
+
+(defn read-data!
+  [file]
+  (->> file
+       slurp
+       string/split-lines
+       (map read-string)))
+
 (defn frequency
   [data]
-  (->> data
-       string/split-lines
-       (map read-string)
-       (apply +)))
+  (apply + data))
 
-(frequency (slurp "data/day-1"))
+(frequency (read-data! "data/day-1"))
+
+;; part 2
+
+(defn first-duplicate-frequency
+  [data]
+  (loop [visited #{0}
+         current-frequency 0
+         r (cycle data)]
+    (let [next-frequency (+ current-frequency
+                            (first r))]
+      (if (contains? visited
+                     next-frequency)
+        next-frequency
+        (recur (conj visited next-frequency)
+               next-frequency
+               (rest r))))))
+
+(first-duplicate-frequency (read-data! "data/day-1"))
